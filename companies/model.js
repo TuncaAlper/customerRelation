b43cf708207e6b73e175ae5b1333971fe82d69c1
Bencilki <alpertunca@gmail.com>
2019-03-13 14:09:46 +0100

    companies added and setup relation with customers

---
+++ companies/model.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../db')
+
+
+
+const Company = sequelize.define('companies', {
+  name: {
+    type: Sequelize.STRING,
+    field: 'name',
+    allowNull: false
+  },
+  foundingYear: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    field: 'description'
+  }
+},{
+    timestamps: false,
+    tableName: 'companies'
+})
+
+module.exports = Company
