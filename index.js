a7f0ae11496e5cb41ef6a21fb04e64952d546e7f
Bencilki <alpertunca@gmail.com>
2019-03-13 17:33:35 +0100

    setup logging and email/password check for user

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser')
 const customersRouter = require('./customers/routes')
 const companyRouter = require('./companies/routes')
 const loginRouter = require('./auth/routes')
+const usersRouter = require('./users/routes')
 
 const app = express()
 const port = process.env.PORT || 4000
 
 app
   .use(bodyParser.json())
-  .use(customersRouter, companyRouter, loginRouter)
+  .use(customersRouter, companyRouter, loginRouter, usersRouter)
   .listen(port, () => console.log(`Listening on port ${port}`))
 
